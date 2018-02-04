7d0087e952c2f911569561670c15a371b721f42f
KID <hjh010501@naver.com>
2018-02-05 01:53:18 +0900

    first init

---
+++ index.php
@@ -0,0 +1,136 @@
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="http://idangero.us/swiper/dist/css/swiper.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<title>DIMINEWS</title>
+</head>
+
+<body>
+    <div class="logo">LOGO</div>
+    <div class="slide">
+    <div class="swiper-container">
+        <div class="swiper-wrapper">
+          <div style="background-image: url(css/img/test.png); background-position: center; background-size:cover;" class="swiper-slide"></div>
+          <div style="background-image: url(css/img/test.png); background-position: center; background-size:cover;" class="swiper-slide"></div>
+          <div style="background-image: url(css/img/test.png); background-position: center; background-size:cover;" class="swiper-slide"></div>
+          <div style="background-image: url(css/img/test.png); background-position: center; background-size:cover; class="swiper-slide"></div>
+        </div>
+        <div class="swiper-pagination"></div>
+      </div>
+    </div>
+    <div class="news_grid">
+       <div class="one news_catagory">
+           <div class="news_top">
+                <div class="news_top_title">학교·라이프</div>
+                <div class="news_more">더보기</div>
+           </div>
+           <div class="news_titles">
+                <ui>
+                    <li>신문부 서버 지원 안해줌?;;</li>
+                    <li>문성환바보</li>
+                    <li>신문부 서버 지원 안해줌?;;</li>
+                    <li>문성환바보</li>
+                    <li>신문부 서버 지원 안해줌?;;</li>
+                </ui>
+           </div>
+       </div>
+       <div class="two news_catagory">
+            <div class="news_top">
+                 <div class="news_top_title">행사</div>
+                 <div class="news_more">더보기</div>
+            </div>
+            <div class="news_titles">
+                 <ui>
+                     <li>신문부 서버 지원 안해줌?;;</li>
+                     <li>문성환바보</li>
+                     <li>신문부 서버 지원 안해줌?;;</li>
+                     <li>문성환바보</li>
+                     <li>신문부 서버 지원 안해줌?;;</li>
+                 </ui>
+            </div>
+        </div>
+        <div class="three news_catagory">
+                <div class="news_top">
+                     <div class="news_top_title">교육·진로</div>
+                     <div class="news_more">더보기</div>
+                </div>
+                <div class="news_titles">
+                     <ui>
+                         <li>신문부 서버 지원 안해줌?;;</li>
+                         <li>문성환바보</li>
+                         <li>신문부 서버 지원 안해줌?;;</li>
+                         <li>문성환바보</li>
+                         <li>신문부 서버 지원 안해줌?;;</li>
+                     </ui>
+                </div>
+            </div>
+            <div class="four news_catagory">
+                    <div class="news_top">
+                         <div class="news_top_title">정치·사회</div>
+                         <div class="news_more">더보기</div>
+                    </div>
+                    <div class="news_titles">
+                         <ui>
+                             <li>신문부 서버 지원 안해줌?;;</li>
+                             <li>문성환바보</li>
+                             <li>신문부 서버 지원 안해줌?;;</li>
+                             <li>문성환바보</li>
+                             <li>신문부 서버 지원 안해줌?;;</li>
+                         </ui>
+                    </div>
+                </div>
+                <div class="five news_catagory">
+                     <div class="news_top">
+                          <div class="news_top_title">IT</div>
+                          <div class="news_more">더보기</div>
+                     </div>
+                     <div class="news_titles">
+                          <ui>
+                              <li>신문부 서버 지원 안해줌?;;</li>
+                              <li>문성환바보</li>
+                              <li>신문부 서버 지원 안해줌?;;</li>
+                              <li>문성환바보</li>
+                              <li>신문부 서버 지원 안해줌?;;</li>
+                          </ui>
+                     </div>
+                 </div>
+                 <div class="six news_catagory">
+                         <div class="news_top">
+                              <div class="news_top_title">기획</div>
+                              <div class="news_more">더보기</div>
+                         </div>
+                         <div class="news_titles">
+                              <ui>
+                                  <li>신문부 서버 지원 안해줌?;;</li>
+                                  <li>문성환바보</li>
+                                  <li>신문부 서버 지원 안해줌?;;</li>
+                                  <li>문성환바보</li>
+                                  <li>신문부 서버 지원 안해줌?;;</li>
+                              </ui>
+                         </div>
+                     </div>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <script src="http://idangero.us/swiper/dist/js/swiper.min.js"></script>
+    <script src="js/main.js"></script>
+    
+    <script>
+        var swiper = new Swiper('.swiper-container', {
+          loop: true,
+          autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
+            },
+          pagination: {
+            el: '.swiper-pagination',
+            dynamicBullets: true,
+          },
+        });
+      </script>
+
+</body>
+</html>
