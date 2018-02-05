c46ef6b6f793dd93b232eecad5d1d85628fa4c89
KID <hjh010501@naver.com>
2018-02-06 02:02:28 +0900

    upload secert connection.php

---
+++ upload/savepost.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: KID
+ * Date: 2018. 2. 6.
+ * Time: 오전 2:00
+ */
---
+++ view_list.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: KID
+ * Date: 2018. 2. 5.
+ * Time: 오전 1:55
+ */
